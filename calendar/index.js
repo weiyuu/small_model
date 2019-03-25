f610a77725ec5c531025f7eeaed1b6a153728af5
yuu <weiyuu95@gmail.com>
2019-03-25 14:50:11 +0800

    添加返回今天按钮

--- calendar/index.js
+++ calendar/index.js
@@ -10,15 +10,15 @@ var month_name = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sep","Oct",
 var holder = document.getElementById("days");
 var prev = document.getElementById("prev");
 var next = document.getElementById("next");
-var ctitle = document.getElementById("calendar-month");
-var cyear = document.getElementById("calendar-year");
+var cMonth = document.getElementById("calendar-month");
+var cYear = document.getElementById("calendar-year");
 
 // 获取当前日期
-var my_date = new Date();
+var now_date = new Date();
 // 获取当前日期的年、月、日
-var my_year = my_date.getFullYear(),
-    my_month = my_date.getMonth(),
-    my_day = my_date.getDate();
+var now_year = now_date.getFullYear(),
+    now_month = now_date.getMonth(),
+    now_day = now_date.getDate();
 
 
 
@@ -41,20 +41,20 @@ function daysMonth(month,year) {
 
 
 // 创建函数生成月份显示
-function refreshDate(my_year,my_month,my_day) {
+function refreshDate(now_year,now_month,now_day) {
     var str = "";
-    var totalDay = daysMonth(my_month,my_year);
+    var totalDay = daysMonth(now_month,now_year);
     
-    var firstDay = dayStart(my_month,my_year);
+    var firstDay = dayStart(now_month,now_year);
 
     var myclass;
     for (var i=0;i<firstDay;i++) {
         str+="<li></li>";
     }
     for(var i=1;i<totalDay;i++){
-        if((i<my_day && my_year==my_date.getFullYear() && my_month==my_date.getMonth()) || my_year<my_date.getFullYear() || ( my_year==my_date.getFullYear() && my_month<my_date.getMonth())){ 
+        if((i<now_day && now_year==now_date.getFullYear() && now_month==now_date.getMonth()) || now_year<now_date.getFullYear() || ( now_year==now_date.getFullYear() && now_month<now_date.getMonth())){ 
 			myclass = " class='lightgrey'"; //当该日期在今天之前时，以浅灰色字体显示
-		}else if (i==my_day && my_year==my_date.getFullYear() && my_month==my_date.getMonth()){
+		}else if (i==now_day && now_year==now_date.getFullYear() && now_month==now_date.getMonth()){
 			myclass = " class='green greenbox'"; //当天日期以绿色背景突出显示
 		}else{
 			myclass = " class='darkgrey'"; //当该日期在今天之后时，以深灰字体显示
@@ -62,30 +62,52 @@ function refreshDate(my_year,my_month,my_day) {
 		str += "<li"+myclass+">"+i+"</li>"; //创建日期节点
     }
     holder.innerHTML = str;
-    ctitle.innerHTML = month_name[my_month];
-    cyear.innerHTML = my_year;
+    cMonth.innerHTML = month_name[now_month];
+    cYear.innerHTML = now_year;
 }
-refreshDate(my_year,my_month,my_day);
+refreshDate(now_year,now_month,now_day);
 
 
 prev.onclick = function(event) {
     event.preventDefault();
-    my_month-=1;
-    if(my_month < 0) {
-        my_year-=1;
-        my_month = 11;
+    now_month-=1;
+    if(now_month < 0) {
+        now_year-=1;
+        now_month = 11;
     }
-    refreshDate(my_year,my_month,my_day);
+    refreshDate(now_year,now_month,now_day);
 }
 
 next.onclick = function(event) {
     event.preventDefault();
-    my_month+=1;
-
-    if( my_month >11) {
-        my_year+=1;
-        my_month=0;
+    now_month+=1;
+    if( now_month >11) {
+        now_year+=1;
+        now_month=0;
     }
-    refreshDate(my_year,my_month,my_day)
+    refreshDate(now_year,now_month,now_day)
 }
+
+// 返回今天按钮
+var today = document.getElementById("today");
+
+today.onclick = function() {
+    // 判断是否是同一年
+    if(cYear.innerText == now_date.getFullYear()) {
+        // 判断是否是同一月，并重置年、月
+        if(cMonth.innerText != month_name[now_date.getMonth()]){ 
+            refreshDate(now_date.getFullYear(),now_date.getMonth(),now_day);
+            now_year = now_date.getFullYear();
+            now_month = now_date.getMonth();
+        }
+        // 不是同一年处理逻辑，并重置年、月
+    }else {
+        refreshDate(now_date.getFullYear(),now_date.getMonth(),now_day);
+        now_year = now_date.getFullYear();
+        now_month = now_date.getMonth();
+    }
+}
+
+
+
 
